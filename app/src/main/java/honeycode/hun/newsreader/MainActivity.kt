9049404af2dcfc2a1276671e57274ec11db08b3a
Jeon <wmf85276@gmail.com>
2020-03-28 23:20:04 +0900

    뉴스 리스트 업데이트 성능 개선중

--- app/src/main/java/honeycode/hun/newsreader/MainActivity.kt
+++ app/src/main/java/honeycode/hun/newsreader/MainActivity.kt
@@ -8,12 +8,14 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
 import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
 import org.w3c.dom.NodeList
 import java.io.InputStream
 import java.io.InputStreamReader
 import java.lang.IndexOutOfBoundsException
 import java.net.HttpURLConnection
 import java.net.URL
+import java.net.URLEncoder
 import javax.xml.parsers.DocumentBuilder
 import javax.xml.parsers.DocumentBuilderFactory
 
@@ -31,27 +33,68 @@ class MainActivity : AppCompatActivity() {
         news_list.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         news_list.adapter = newsListAdapter
 
+        val newsDocs = ArrayList<Document>()
         GlobalScope.launch(Dispatchers.Main) {
             val httpCoroutine = async(Dispatchers.IO) {
                 val inputStream: InputStream = getInputStream(URL("https://news.google.com/rss?hl=ko&gl=KR&ceid=KR:ko"))
                 val docBuilder: DocumentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder()
                 val nodeList: NodeList = docBuilder.parse(inputStream).documentElement.getElementsByTagName("item")
-
-                for(i in 0..6) {  // 에러 잡기
+                for(i in 0 until 10) {  // 완료된 아이템 부터 리스트 업하는 방법 검토
                     try {
+                        Log.d("TAG", "IO Index: $i")
                         val newsArticleUrl: String = nodeList.item(i).childNodes.item(1).childNodes.item(0).nodeValue
-                        val newsArticleHtml: String = loadHtml(URL(newsArticleUrl))
-                        val jsoupDocument = Jsoup.parse(newsArticleHtml)
-                        val newsThumbnailUrl = jsoupDocument.select("meta[property=og:image]")[0].attr("content")
-                        val newsTitle: String = nodeList.item(i).childNodes.item(0).childNodes.item(0).nodeValue
-                        val newsDescription = jsoupDocument.select("meta[property=og:description]")[0].attr("content")
-                        addNewsItem(newsThumbnailUrl, newsTitle, newsDescription)
+                        newsDocs.add(Jsoup.connect(newsArticleUrl).get())
+//                        val newsThumbnailUrl = jsoupDocument.select("meta[property=og:image]")[0].attr("content")
+//                        val newsTitle: String = nodeList.item(i).childNodes.item(0).childNodes.item(0).nodeValue
+//                        val newsDescription = jsoupDocument.select("meta[property=og:description]")[0].attr("content")
                     } catch (e: IndexOutOfBoundsException) {
                         e.printStackTrace()
                     }
                 }
             }
+            val testCoroutine = async(Dispatchers.Default) {
+                try {
+                    var i  = 0
+                    while (true) {
+                        if(newsDocs.size > newsListAdapter.itemCount) {
+                            Log.d("TAG", "Test Index: $i")
+                            val newsThumbnailUrl = newsDocs[i].select("meta[property=og:image]")[0].attr("content")
+                            val newsDescription = newsDocs[i].select("meta[property=og:description]")[0].attr("content")
+                            addNewsItem(newsThumbnailUrl, "TEST", newsDescription)
+//                            newsListAdapter.notifyDataSetChanged()
+                            i += 1
+                        }
+
+                        if(i >= 9) break
+                    }
+//                    for(newsDoc in newsDocs) {
+//                        val newsThumbnailUrl = newsDoc.select("meta[property=og:image]")[0].attr("content")
+//                        val newsDescription = newsDoc.select("meta[property=og:description]")[0].attr("content")
+//                        addNewsItem(newsThumbnailUrl, "TEST", newsDescription)
+//                    }
+                } catch (e: IndexOutOfBoundsException) {e.printStackTrace()}
+//                val newsThumbnailUrl = newsArticleUrlList[0].select("meta[property=og:image]")[0].attr("content")
+//                val newsDescription = newsArticleUrlList[0].select("meta[property=og:description]")[0].attr("content")
+            }
+            testCoroutine.await()
+
+//            val ioCoroutine2 = async(Dispatchers.IO) {
+//                val inputStream: InputStream = getInputStream(URL("https://news.google.com/rss?hl=ko&gl=KR&ceid=KR:ko"))
+//                val docBuilder: DocumentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder()
+//                val nodeList: NodeList = docBuilder.parse(inputStream).documentElement.getElementsByTagName("item")
+//
+//                for(i in 10 until 20) {
+//                    try {
+//                        Log.d("TAG", "Index: $i")
+//                        val newsArticleUrl: String = nodeList.item(i).childNodes.item(1).childNodes.item(0).nodeValue
+//                        newsDocs.add(Jsoup.connect(newsArticleUrl).get())
+//                    } catch (e: IndexOutOfBoundsException) {
+//                        e.printStackTrace()
+//                    }
+//                }
+//            }
             httpCoroutine.await()
+//            ioCoroutine2.await()
             Log.d("TAG", "item count: ${newsListAdapter.itemCount}")
             newsListAdapter.notifyDataSetChanged()
         }
@@ -66,7 +109,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun getInputStream(url: URL): InputStream {
         var conn: HttpURLConnection = url.openConnection() as HttpURLConnection
-        conn.connectTimeout = 5000
+        conn.requestMethod = "GET"
         Log.d("TAG", "Response Code: ${conn.responseCode}")
 
         return when(conn.responseCode) {
@@ -74,6 +117,7 @@ class MainActivity : AppCompatActivity() {
             HttpURLConnection.HTTP_MOVED_TEMP, HttpURLConnection.HTTP_MOVED_PERM -> {
                 val redirectedUrl = conn.getHeaderField("Location")
                 conn = URL(redirectedUrl).openConnection() as HttpURLConnection
+                conn.requestMethod = "GET"
                 conn.inputStream
             }
             else -> conn.inputStream
@@ -82,7 +126,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun loadHtml(url: URL): String {
         var conn = url.openConnection() as HttpURLConnection
-        conn.connectTimeout = 5000
+        conn.requestMethod = "GET"
         Log.d("TAG", "Http Response Code: ${conn.responseCode}")
 
         return when(conn.responseCode) {
@@ -96,6 +140,7 @@ class MainActivity : AppCompatActivity() {
             HttpURLConnection.HTTP_MOVED_TEMP, HttpURLConnection.HTTP_MOVED_PERM -> {
                 val redirectedUrl: String = conn.getHeaderField("Location")
                 conn = URL(redirectedUrl).openConnection() as HttpURLConnection
+                conn.requestMethod = "GET"
 
                 val bufferedReader = InputStreamReader(conn.inputStream, "utf-8")
                 val buffer = StringBuffer()
